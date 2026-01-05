d5a9f041df06751b6ff563021e5aaed70df894be
Choakthawee <140490522+Choakthawee@users.noreply.github.com>
2026-01-05 19:18:02 +0700

    first push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,65 +1,55 @@
-import Image from "next/image";
+import ClickSpark from "@/components/click-spark/ClickSpark";
+import Particles from "@/components/particle-bg/Particles";
+import ScrambledText from "@/components/scrambled-text/ScrambledText";
+import Shuffle from "@/components/shuffle-text/ShuffleText";
 
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    <ClickSpark
+      sparkColor='#fff'
+      sparkSize={10}
+      sparkRadius={15}
+      sparkCount={8}
+      duration={400}>
+
+      <div className="relative w-full h-screen overflow-hidden bg-black">
+
+        <div className="absolute inset-0 z-0">
+          <Particles
+            particleColors={['#ffffff', '#ffffff']}
+            particleCount={200}
+            particleSpread={10}
+            speed={0.1}
+            particleBaseSize={100}
+            moveParticlesOnHover={true}
+            alphaParticles={false}
+            disableRotation={false}
+          />
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+
+        <div className="flex flex-col z-10 items-center justify-center h-full w-full px-4 gap-0">
+
+          <ScrambledText
+            className="text-white text-center text-4xl md:text-6xl font-bold font-mono tracking-wider"
+            radius={100}
+            duration={1.2}
+            speed={0.5}
+            scrambleChars=".:"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+            PORTFOLIO
+          </ScrambledText>
+
+          <Shuffle
+            className="text-gray-300 text-center text-lg md:text-2xl font-mono"
+            duration={0.65}
+            shuffleTimes={2}
+            loop={true}
+            loopDelay={1.5}
+            text="Presented By Choakthawee Nikhomkhai"
+          />
+
         </div>
-      </main>
-    </div>
+      </div>
+    </ClickSpark>
   );
 }
