42a2f38ed75aacfe33dc3feb75964154db7ee965
Choakthawee <140490522+Choakthawee@users.noreply.github.com>
2026-01-05 20:57:57 +0700

    update pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import ClickSpark from "@/components/click-spark/ClickSpark";
 import Particles from "@/components/particle-bg/Particles";
 import ScrambledText from "@/components/scrambled-text/ScrambledText";
 import Shuffle from "@/components/shuffle-text/ShuffleText";
+import StarBorder from "@/components/star-border/StarBorder";
 
 export default function Home() {
   return (
@@ -27,14 +28,14 @@ export default function Home() {
           />
         </div>
 
-        <div className="flex flex-col z-10 items-center justify-center h-full w-full px-4 gap-0">
+        <div className="flex flex-col z-10 items-center justify-center h-full w-full px-4 gap-6">
 
           <ScrambledText
             className="text-white text-center text-4xl md:text-6xl font-bold font-mono tracking-wider"
             radius={100}
             duration={1.2}
             speed={0.5}
-            scrambleChars=".:"
+            scrambleChars="AZX!@938AISJDLASKJDX>CN"
           >
             PORTFOLIO
           </ScrambledText>
@@ -47,6 +48,15 @@ export default function Home() {
             loopDelay={1.5}
             text="Presented By Choakthawee Nikhomkhai"
           />
+
+          <StarBorder
+            as="button"
+            className="custom-class cursor-pointer"
+            color="cyan"
+            speed="5s"
+          >
+            Let's Start
+          </StarBorder>
 
         </div>
       </div>
